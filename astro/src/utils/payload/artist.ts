9fe05c40d8539f50c75520c5e2958e8f86c7b48e
Luke MacKenzie <lmackenzie@reli.sh>
2023-09-07 15:13:57 -0400

    add Similar Artists section

--- astro/src/utils/payload/artist.ts
+++ astro/src/utils/payload/artist.ts
@@ -16,3 +16,52 @@ export const getArtistBySlug = async (slug: string) => {
     await fetch(`${URL}/api/artists?where[slug][equals]=${slug}`)
   ).json();
 };
+
+export const getRecordsBySimilarGenre = async (
+  record: Record,
+  limit: number = 3
+) => {
+  const { id, genres: genresArray } = record;
+
+  const genres = genresArray?.map((genre) => genre.id).join(',');
+
+  try {
+    const similarRecords = (
+      await (
+        await fetch(
+          `${URL}/api/records?where[genres][in]=${genres}&where[id][not_equals]=${id}&limit=${limit}`
+        )
+      ).json()
+    ).docs;
+
+    return similarRecords as Record[];
+  } catch (e) {
+    console.log(e);
+
+    return [];
+  }
+};
+export const getArtistsBySimilarGenre = async (
+  artist: Artist,
+  limit: number = 3
+) => {
+  const { id, genres: genresArray } = artist;
+
+  const genres = genresArray?.map((genre) => genre.id).join(',');
+
+  try {
+    const similarArtists = (
+      await (
+        await fetch(
+          `${URL}/api/artists?where[genres][in]=${genres}&where[id][not_equals]=${id}&limit=${limit}`
+        )
+      ).json()
+    ).docs;
+
+    return similarArtists as Artist[];
+  } catch (e) {
+    console.log(e);
+
+    return [];
+  }
+};
